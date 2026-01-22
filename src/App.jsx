bf78df3095e5d68823328e3829f42c0974739e5a
Jose Ortega <designlol80@gmail.com>
2026-01-21 23:14:39 -0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
       {/* Aquí cada quien llama a su componente el que va a trabajar....*/}
       {/* ejemplo: <Login></Login> ó <Registrar></Registrar> ó <RecuperarContrasena></RecuperarContrasena>*/}
       {/* Asi puede cada uno ver su pantalla sin necesidad de esperar a que algun compañero termine para avanzar*/}
-      <Login></Login>
     </div>
   )
 }
