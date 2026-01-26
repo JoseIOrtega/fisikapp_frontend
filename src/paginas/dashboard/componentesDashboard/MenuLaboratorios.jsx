89090b6e9101de4aad8f9daa90d06178ce349b68
cristian  Urrutia <urrutiacristian86@gmail.com>
2026-01-25 22:27:49 -0500

    commit para agregar boton de crear y unirse

--- src/paginas/dashboard/componentesDashboard/MenuLaboratorios.jsx
+++ src/paginas/dashboard/componentesDashboard/MenuLaboratorios.jsx
@@ -6,15 +6,46 @@ import { useState } from 'react';
 function MenuLaboratorios() {
 
   const [mostrarModalUnirse, setMostrarModalUnirse] = useState(false);
+  const [mostrarOpciones, setMostrarOpciones] = useState(false);
+
 
 
   return (
-    <div>
+    
+    <div className="acciones-menu">
+      <br />
+  <button
+    className="btn-add"
+    title="Add New"
+    onClick={() => setMostrarOpciones(!mostrarOpciones)}
+  >
+    <svg
+      className="btn-icon"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        className="circle"
+        d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+      />
+      <path className="line" d="M8 12H16" />
+      <path className="line" d="M12 16V8" />
+    </svg>
+  </button>
 
-        aqui Va imagen y configuracion del menu
+  {mostrarOpciones && (
+    <div className="dropdown-menu">
+      <button onClick={() => alert("Crear laboratorio")}>
+        Crear
+      </button>
 
-        
-        <UnirseLaboratorioModal visible={mostrarModalUnirse} 
+      <button onClick={() => alert("Unirse a laboratorio")}>
+        Unirse
+      </button>
+    </div>
+  )}
+
+    <UnirseLaboratorioModal visible={mostrarModalUnirse} 
           onClose={() => setMostrarModalUnirse(false)} 
           onSubmit={(e) => {e.preventDefault();
                             setMostrarModalUnirse(false);}
